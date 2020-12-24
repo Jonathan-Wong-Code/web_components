bee2d517debb1245dfb8b48124a8295975cff9b4
Jonathan Wong <jonathan.wong@applydigital.co>
2020-12-23 20:55:48 -0500

    working

--- src/components/CodeInput/CodeInput.tsx
+++ src/components/CodeInput/CodeInput.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useEffect, useState, ChangeEvent } from 'react';
+import { ScreenReaderText } from '../ScreenReaderText/ScreenReaderText';
 interface ICodeInput {
   size: number;
   defaultValue?: string;
@@ -10,20 +10,47 @@ interface ICodeInput {
 export const CodeInput = ({ size = 5, defaultValue, placeholder, onChange }: ICodeInput) => {
 
   const defaultValueArray = defaultValue?.split('');
+  const [inputValues, setInputValues] = useState(defaultValueArray ? defaultValueArray : new Array(size).fill(''));
 
-  const [inputValues, setInputValues] = React.useState(defaultValueArray ? defaultValueArray : new Array(size).fill(''));
-
-  React.useEffect(() => {
+  useEffect(() => {
     onChange(inputValues.join(''));
   }, [inputValues, onChange])
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, index: number) => {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>, index: number) => {
     const element = e.target;
     const newInputValue = e.target.value;
+
     if (isNaN(Number(newInputValue))) return;
+
     setInputValues(prevState => prevState.map((currentInputValue, i) => i === index ? newInputValue : currentInputValue))
 
-    if (element.nextSibling) (element.nextSibling as HTMLElement).focus();
+    //@ts-ignore
+    if (element.nextSibling && e.nativeEvent.inputType !== "deleteContentBackward") {
+      (element.nextSibling as HTMLElement).focus();
+    }
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent, index: number) => {
+    const element = e.target as HTMLElement;
+
+    if (e.key === inputValues[index]) {
+      e.preventDefault(); // Prevents handlechange from firing if the same number is entered and we just focus next input
+      (element.nextSibling as HTMLElement).focus();
+    }
+
+    if (e.key === 'ArrowRight') {
+      if (element.nextSibling) {
+        setTimeout(() => (element.nextSibling as HTMLElement).focus(), 0);
+      }
+    }
+
+    if (e.key === 'ArrowLeft') {
+      setTimeout(() => (element.previousSibling?.previousSibling as HTMLElement).focus(), 0)
+    }
+
+    if (e.key === 'ArrowUp') e.preventDefault();
+
+    if (e.key === 'ArrowDown') e.preventDefault();
   }
 
   return (
@@ -31,16 +58,26 @@ export const CodeInput = ({ size = 5, defaultValue, placeholder, onChange }: ICo
       {
         inputValues.map((data, index) => {
           return (
-            <input
-              className='codeInput'
-              type='text'
-              name='code'
-              placeholder={placeholder}
-              defaultValue={defaultValueArray ? defaultValueArray[index] : undefined}
-              maxLength={1}
-              onChange={e => handleChange(e, index)}
-              onFocus={e => e.target.select()}
-            />
+            <>
+              <label htmlFor={`code-input-${index}`}>
+                <ScreenReaderText>
+                  Enter number at position {index + 1}
+                </ScreenReaderText>
+              </label>
+              <input
+                className='codeInput'
+                type='text'
+                name='code'
+                id={`code-input-${index}`}
+                placeholder={placeholder}
+                defaultValue={defaultValueArray ? defaultValueArray[index] : undefined}
+                maxLength={1}
+                value={data}
+                onChange={e => handleChange(e, index)}
+                onFocus={e => e.target.select()}
+                onKeyDown={e => handleKeyDown(e, index)}
+              />
+            </>
           )
         })
       }
---
+++ src/components/ScreenReaderText/ScreenReaderText.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ScreenReaderText = styled.span`
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  whitespace: nowrap;
+  width: 1px;
+`;
