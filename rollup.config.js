8226bff599b1469194f0ac3b11f6e80fac5adeaf
Jonathan Wong <jonathan.wong@applydigital.co>
2021-01-27 14:47:12 -0500

    before trying multi input

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
 
+/* eslint-disable  @typescript-eslint/no-var-requires */
 const packageJson = require('./package.json');
 
 export default {
